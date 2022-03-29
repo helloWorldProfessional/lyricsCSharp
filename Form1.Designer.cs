d049757209fd3ad178f1192490b950cb2748602a
Sanja <sanjacvijetic21@gmail.com>
2022-03-29 17:50:15 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace CsharpLyrics
+{
+    partial class Lyrics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Lyrics));
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ArtistTextBox = new System.Windows.Forms.TextBox();
+            this.SongTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.SearchButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SearchButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.SearchButton.Image = ((System.Drawing.Image)(resources.GetObject("SearchButton.Image")));
+            this.SearchButton.Location = new System.Drawing.Point(409, 33);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(147, 62);
+            this.SearchButton.TabIndex = 0;
+            this.SearchButton.Text = "PRETRAGA";
+            this.SearchButton.UseVisualStyleBackColor = false;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(68, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IZVOĐAČ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PJESMA";
+            // 
+            // ArtistTextBox
+            // 
+            this.ArtistTextBox.Location = new System.Drawing.Point(150, 31);
+            this.ArtistTextBox.Name = "ArtistTextBox";
+            this.ArtistTextBox.Size = new System.Drawing.Size(239, 22);
+            this.ArtistTextBox.TabIndex = 4;
+            // 
+            // SongTextBox
+            // 
+            this.SongTextBox.Location = new System.Drawing.Point(150, 73);
+            this.SongTextBox.Name = "SongTextBox";
+            this.SongTextBox.Size = new System.Drawing.Size(239, 22);
+            this.SongTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(691, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(34, 122);
+            this.label4.MinimumSize = new System.Drawing.Size(550, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(550, 17);
+            this.label4.TabIndex = 7;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label4.Visible = false;
+            // 
+            // Lyrics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(625, 397);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SongTextBox);
+            this.Controls.Add(this.ArtistTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SearchButton);
+            this.Name = "Lyrics";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lyrics";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ArtistTextBox;
+        private System.Windows.Forms.TextBox SongTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace CsharpLyrics
+{
+    public partial class Lyrics : Form
+    {
+        public Lyrics()
+        {
+            InitializeComponent();
+        }
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            string artistName = ArtistTextBox.Text;
+            string songTitle = SongTextBox.Text;
+            if (artistName == "" || songTitle == "")
+                MessageBox.Show("Unesite oba polja!");
+            else {
+                try
+                {
+                    string apiUrl = String.Format("https://api.lyrics.ovh/v1/" + artistName + "/" + songTitle);
+                    WebClient client = new WebClient();
+                    string jResponse = client.DownloadString(apiUrl);
+                    dynamic dobj = JsonConvert.DeserializeObject<dynamic>(jResponse);
+                    string lyrics = dobj["lyrics"].ToString();
+                    label4.Text = lyrics;
+                    label4.Visible=true;
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Traženi tekst nije pronađen");
+                }
+            }
+        }
+    }
+}
